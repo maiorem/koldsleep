a30ba863dcc699125df7042ad82883e944492d00
Seyoung Hong <maiorem00@gmail.com>
2021-10-28 00:16:32 +0900

    Update form.py

--- dream_list/form.py
+++ dream_list/form.py
@@ -4,8 +4,9 @@ from dream_list.models import Board
 class BoardForm(forms.ModelForm) :
     class Meta :
         model = Board
-        fields = ['title', 'content', 'writer', 'password']
+        fields = ['cdate', 'title', 'content', 'writer', 'password']
         labels = {
+            'cdate' : '날짜',
             'title' : '제목',
             'content' : '내용',
             'writer' : '이름',
