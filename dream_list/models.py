bea4889148968b0f0b6885f0189d69c5b1e13146
Seyoung Hong <maiorem00@gmail.com>
2021-11-10 22:34:32 +0900

    timezone

--- dream_list/models.py
+++ dream_list/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from pytz import timezone
 
 # Create your models here.
 class Board(models.Model) :
@@ -7,6 +9,11 @@ class Board(models.Model) :
     writer = models.CharField(max_length=10)
     password = models.CharField(max_length=20)
     cdate = models.DateTimeField()
+
+    @property
+    def cdate_korean_time(self):
+        korean_timezone = timezone(settings.TIME_ZONE)
+        return self.cdate.astimezone(korean_timezone)
 
     def __str__ (self) :
         return self.title
